23fdadd263339f45b35a30202d5f802f1258f1cc
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-11-30 19:00:19 -0700

    feat: highlight ships
    Shows the players ships on the grid

--- dom-actions.js
+++ dom-actions.js
@@ -1,6 +1,6 @@
 import { Ship, Gameboard, Player } from "./script.js";
 
-function makeSquares() {
+function makeSquares(name) {
   const boardDiv = document.createElement("div");
   boardDiv.classList.add("board");
   for (let y = 0; y < 10; y++) {
@@ -8,7 +8,7 @@ function makeSquares() {
       const square = document.createElement("div");
       square.classList.add("square");
       //id corresponds with array
-      square.id = `[${y}][${x}]`;
+      square.id = `[${y}][${x}]${name}`;
       boardDiv.appendChild(square);
     }
   }
@@ -18,11 +18,21 @@ function makeSquares() {
 const playerBoard = document.getElementById("playerBoard");
 const computerBoard = document.getElementById("computerBoard");
 
-playerBoard.appendChild(makeSquares());
-computerBoard.appendChild(makeSquares());
+playerBoard.appendChild(makeSquares("player"));
+computerBoard.appendChild(makeSquares("computer"));
 
 const player = new Player();
 const computer = new Player();
 
+player.gameboard.placeShips();
+computer.gameboard.placeShips();
+
 //hightlight ships
-//if (player.gameboard.coords[y][x] === ship)
+for (let y = 0; y < 10; y++) {
+  for (let x = 0; x < 10; x++) {
+    if (player.gameboard.coords[y][x] !== 0) {
+      const highlightShip = document.getElementById(`[${y}][${x}]player`);
+      highlightShip.style.backgroundColor = "red";
+    }
+  }
+}
